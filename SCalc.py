e4d397c1d94f47ffb3775810de261f4b775222fa
Petre Razvan <petre.ispir2002@protonmail.com>
2025-12-15 20:17:59 +0200

    Update SCalc.py

--- SCalc.py
+++ SCalc.py
@@ -45,9 +45,7 @@ def equation():
     current_equation = equationText.get()
     equationText.delete(0, END)
     try:
-        # dont type:
-        # __import__('os').system('reboot')
-        result = eval(current_equation)
+        result = eval(current_equation)  # dont type: __import__('os').system('reboot')
         equationText.insert(END, result)
     except (ValueError, SyntaxError, ZeroDivisionError) as error:
         equationText.insert(END, f"Invalid equation: {error}")
