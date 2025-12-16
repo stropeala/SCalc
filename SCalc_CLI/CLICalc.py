4953c74bb2a3a1c26e6f3655cfcf1dcaa7d5cb77
Petre Razvan <petre.ispir2002@protonmail.com>
2025-12-16 16:58:35 +0200

    Create CLICalc.py

---
+++ SCalc_CLI/CLICalc.py
@@ -0,0 +1,8 @@
+from math_utils import addition, division, multiplication, percentage, subtraction
+
+if __name__ == "__main__":
+    print(addition(10, 2))
+    print(subtraction(10, 2))
+    print(multiplication(10, 2))
+    print(division(10, 2))
+    print(percentage(50, 144))
