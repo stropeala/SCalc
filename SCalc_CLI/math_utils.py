91e93ee4c3aeff90e615b8936624fa24eb7e2d2f
Petre Razvan <petre.ispir2002@protonmail.com>
2025-12-17 13:01:48 +0200

    Update math_utils.py

--- SCalc_CLI/math_utils.py
+++ SCalc_CLI/math_utils.py
@@ -23,10 +23,3 @@ def division(a: float, b: float) -> float:
 def percentage(percent: float, b: float) -> float:
     # We made a function for percentages
     return (percent / 100) * b
-
-
-print(addition(10, 2))
-print(subtraction(10, 2))
-print(multiplication(10, 2))
-print(division(10, 2))
-print(percentage(50, 250))
