10cf1b8d8e87d21db5dcc3df5d63c785a9a0c8b5
Petre Razvan <petre.ispir2002@protonmail.com>
2025-12-17 13:40:03 +0200

    Update CLICalc.py

--- SCalc_CLI/CLICalc.py
+++ SCalc_CLI/CLICalc.py
@@ -1,14 +1,29 @@
 from math_utils import addition, division, multiplication, percentage, subtraction
 from parser import parser
+from shunting_yard_algorithm import shunting_yard_algorithm
 
 if __name__ == "__main__":
+    # Math Utilities test
+    print("#---------------------------------------------------------------------#")
     print(addition(10, 2))
     print(subtraction(10, 2))
     print(multiplication(10, 2))
     print(division(10, 2))
     print(percentage(50, 144))
+
+    # Parser test
+    print("#---------------------------------------------------------------------#")
     print(parser("25% * 250"))
+    print(parser("79 + -5"))
     print(parser("(18/(2+2x2))"))
     print(parser("18/2+2x2"))
     print(parser("1/100+2+3/2"))
-    print(parser(input("Please enter an equation: ")))
+
+    # Shunting Yard Algorithm test
+    print("#---------------------------------------------------------------------#")
+    parsed_equation = parser("(18/(2+2x2))")
+    parsed_equation2 = parser("79 + -5")
+    parsed_equation3 = parser("25% * 250")
+    print(shunting_yard_algorithm(parsed_equation))
+    print(shunting_yard_algorithm(parsed_equation2))
+    print(shunting_yard_algorithm(parsed_equation3))
